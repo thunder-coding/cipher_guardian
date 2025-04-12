16a8f374d876b6d25c13beed5f073bd50f8d7e11
amgothkalyan <amgothkalyan43@gmail.com>
2025-04-12 14:58:10 +0530

    Password page made

--- lib/pages/password_page.dart
+++ lib/pages/password_page.dart
@@ -3,6 +3,55 @@ import 'package:flutter/material.dart';
 class PasswordPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center();
+    return MaterialApp(theme: ThemeData(useMaterial3: true),
+    debugShowCheckedModeBanner: false,
+    home: const AccTiles());
+  }
+}
+
+class AccTiles extends StatelessWidget {
+  const AccTiles({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: ListView(
+        children: const <Widget>[
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.person),
+              title: Text("GitHub"),
+              subtitle: Text("PassWord : **** ****"),
+              trailing: Icon(Icons.more_vert),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Discord"),
+              subtitle: Text("PassWord : **** ****"),
+              trailing: Icon(Icons.more_vert),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Reddit"),
+              subtitle: Text("PassWord : **** ****"),
+              trailing: Icon(Icons.more_vert),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Gmail"),
+              subtitle: Text("PassWord : **** ****"),
+              trailing: Icon(Icons.more_vert),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
